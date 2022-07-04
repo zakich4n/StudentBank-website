f7d4424bb461fd9cbeb296492c479f2302768399
Zaki Gouizi <zaki.gouizi@student.junia.com>
2022-07-04 12:41:45 +0200

    base navbar with link

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,16 +1,71 @@
 import { Link } from "react-router-dom";
+import {
+    Box,
+    Flex,
+    Image,
+    Link as ChakraLink,
+    Stack,
+} from '@chakra-ui/react';
+
+
+const Navigation=[
+    {
+        name : "Banque",
+        link : "/banque"
+    },
+    {
+        name : "Logement",
+        link : "/logement"
+    },
+    {
+        name : "Mobilité",
+        link : "/mobilite"
+    },
+    {
+        name : "Notre mission",
+        link : "/mission"
+    },
+    {
+        name : "L'équipe",
+        link : "/equipe"
+    },
+    {
+        name : "Contact",
+        link : "/contact"
+    }
+]
 
 export default function NavBar() {
     return(
         <>
-        <nav>
-            <Link to="/banque">Banuqe</Link>
-            <Link to="/logement">Logement</Link>
-            <Link to="/mobilite">Mobilité</Link>
-            <Link to="/mission">Notre Mission</Link>
-            <Link to="/equipe">L'équipe</Link>
-            <Link to="/contact">Contact</Link>
-        </nav>
+        <Box 
+            bg='white' 
+            px={10}
+            
+            boxShadow='md'
+            sx={{
+                'font-style': 'normal',
+                'font-weight': 400,
+                'font-size': '23px',
+                'line-height': '27px',
+            }}
+        >
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'} >
+          <Image boxSize='262px' h='100px' pt={3}  src="logo SB.png" />
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'}>
+                <nav>
+                    {Navigation.map(item => (
+                        <ChakraLink pl={12}>
+                            <Link to={item.link}>{item.name}</Link>
+                        </ChakraLink>
+                    ))}
+                </nav>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
         </>
     )
 }
