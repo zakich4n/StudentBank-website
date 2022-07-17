091d662727cad3dd355915704f2b07687b9d5abe
Zaki Gouizi <zaki.gouizi@student.junia.com>
2022-07-17 22:00:04 +0200

    modifie la page equipe

--- src/routes/Equipe.js
+++ src/routes/Equipe.js
@@ -1,225 +1,47 @@
-import { Box } from '@chakra-ui/react'
-import { Link } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
-import { SiLinkedin } from 'react-icons/si';
-import { Grid } from "@chakra-ui/react"
-import { extendTheme } from '@chakra-ui/react'
-import { Heading, Avatar, Flex, Text, Stack, Button, useColorModeValue,} from '@chakra-ui/react';
+import { Box, VStack, SimpleGrid } from '@chakra-ui/react'
 
 
-const breakpoints = {
-  sm: '320px',
-  md: '768px',
-  lg: '960px',
-  xl: '1200px',
-  '2xl': '1536px',
-}
-
-// 3. Extend the theme
-const theme = extendTheme({ breakpoints })
+import Card from '../component/UserCard'
 
 export default function Equipe(){
   return (
     <div>
-        <Box display='block'
-            alignItems='center'
-            justifyContent='center'
+        <Box 
             width='100%'
-            height='100vh'
             bgImage = "url('BG_equipe_light.png')" 
             bgPosition="center" 
             bgRepeat="no-repeat"
             bgSize='cover'>
 
-            <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>  
-            <Grid templateColumns="repeat(5, 1fr)" p={50} gap={0}>
-                <Box p={8}
-                    maxW={'200px'}
-                    w={'full'}
-                    rounded={'md'}
-                    overflow={'hidden'}>
+              <VStack >
+
+                <SimpleGrid columns={{sm: 1, md: 2, lg: 4}} spacing={10} >
+                    {Card('Zaki GOUIZI','@zackch4n',
+                    'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+                    "Une bonne description de merde",
+                    ["front","back","tout"])}
+
+                    {Card('Zaki GOUIZI','@zackch4n',
+                    'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+                    "Une bonne description de merde",
+                    ["front","back","tout"])}  
                     
-                    <Image
-                    h={'100px'}
-                    w={'full'}
-                    src={
-                        'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-                    }
-                    objectFit={'cover'}
-                    />
-                    <br></br>
-                    <Stack spacing={0} align={'center'} mb={5}>
-                        <Heading color={useColorModeValue('black', 'white')}fontSize={'xl'} fontWeight={500} fontFamily={'body'}>
-                        John Doe
-                        </Heading>
-                        <Text color={useColorModeValue('black', 'white')} fontSize={'12'}>Frontend Developer</Text>
-                    </Stack>
+                    {Card('Zaki GOUIZI','@zackch4n',
+                    'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+                    "Une bonne description de merde",
+                    ["front","back","tout"])}
 
-                    <Stack direction={'row'} justify={'center'} spacing={6}>
-                        <Stack align={'center'}>
-                           <Button
-                                variant='ghost'
-                                w={'30'}
-                                mt={2}
-                                rounded={'md'}
-                                leftIcon={<SiLinkedin/>}
-                                _hover={{
-                                transform: 'translateY(-2px)',
-                                boxShadow: 'lg',
-                                }}>
-                            </Button>
-                        </Stack>
-                    </Stack>
-                </Box>             
-                <Box p={8}
-                    maxW={'200px'}
-                    w={'full'}
-                    rounded={'md'}
-                    overflow={'hidden'}> 
-                    <Image
-                    h={'100px'}
-                    w={'full'}
-                    src={
-                        'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-                    }
-                    objectFit={'cover'}
-                    />
-                    <br></br>
-                    <Stack spacing={0} align={'center'} mb={5}>
-                        <Heading color={useColorModeValue('black', 'white')}fontSize={'xl'} fontWeight={500} fontFamily={'body'}>
-                        John Doe
-                        </Heading>
-                        <Text color={useColorModeValue('black', 'white')} fontSize={'12'}>Frontend Developer</Text>
-                    </Stack>
+                    {Card('Zaki GOUIZI','@zackch4n',
+                    'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+                    "Une bonne description de merde",
+                    ["front","back","tout"])}
 
-                    <Stack direction={'row'} justify={'center'} spacing={6}>
-                        <Stack align={'center'}>
-                            <Button
-                                variant='ghost'
-                                w={'30'}
-                                mt={2}
-                                rounded={'md'}
-                                leftIcon={<SiLinkedin/>}
-                                _hover={{
-                                transform: 'translateY(-2px)',
-                                boxShadow: 'lg',
-                                }}>
-                            </Button>
-                        </Stack>
-                    </Stack>
-                </Box>             
-                <Box p={8}
-                    maxW={'200px'}
-                    w={'full'}
-                    rounded={'md'}
-                    overflow={'hidden'}>
-                    <Image
-                    h={'100px'}
-                    w={'full'}
-                    src={
-                        'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-                    }
-                    objectFit={'cover'}
-                    />
-                    <br></br>
-                    <Stack spacing={0} align={'center'} mb={5}>
-                        <Heading color={useColorModeValue('black', 'white')}fontSize={'xl'} fontWeight={500} fontFamily={'body'}>
-                        John Doe
-                        </Heading>
-                        <Text color={useColorModeValue('black', 'white')} fontSize={'12'}>Frontend Developer</Text>
-                    </Stack>
-
-                    <Stack direction={'row'} justify={'center'} spacing={6}>
-                        <Stack align={'center'}>
-                            <Button
-                                variant='ghost'
-                                w={'30'}
-                                mt={2}
-                                rounded={'md'}
-                                leftIcon={<SiLinkedin/>}
-                                _hover={{
-                                transform: 'translateY(-2px)',
-                                boxShadow: 'lg',
-                                }}>
-                            </Button>
-                        </Stack>
-                    </Stack>
-                </Box>
-                 <Box p={8}
-                    maxW={'200px'}
-                    w={'full'}
-                    rounded={'md'}
-                    overflow={'hidden'}>
-                    <Image
-                    h={'100px'}
-                    w={'full'}
-                    src={
-                        'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-                    }
-                    objectFit={'cover'}
-                    />
-                    <br></br>
-                    <Stack spacing={0} align={'center'} mb={5}>
-                        <Heading color={useColorModeValue('black', 'white')}fontSize={'xl'} fontWeight={500} fontFamily={'body'}>
-                        John Doe
-                        </Heading>
-                        <Text color={useColorModeValue('black', 'white')} fontSize={'12'}>Frontend Developer</Text>
-                    </Stack>
-
-                    <Stack direction={'row'} justify={'center'} spacing={6}>
-                        <Stack align={'center'}>
-                            <Button
-                                variant='ghost'
-                                w={'30'}
-                                mt={2}
-                                rounded={'md'}
-                                leftIcon={<SiLinkedin/>}
-                                _hover={{
-                                transform: 'translateY(-2px)',
-                                boxShadow: 'lg',
-                                }}>
-                            </Button>
-                        </Stack>
-                    </Stack>
-                </Box>             
-                 <Box p={8}
-                    maxW={'200px'}
-                    w={'full'}
-                    rounded={'md'}
-                    overflow={'hidden'}>
-                    <Image
-                    h={'100px'}
-                    w={'full'}
-                    src={
-                        'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-                    }
-                    objectFit={'cover'}
-                    />
-                    <br></br>
-                    <Stack spacing={0} align={'center'} mb={5}>
-                        <Heading color={useColorModeValue('black', 'white')}fontSize={'xl'} fontWeight={500} fontFamily={'body'}>
-                        John Doe
-                        </Heading>
-                        <Text color={useColorModeValue('black', 'white')} fontSize={'12'}>Frontend Developer</Text>
-                    </Stack>
-
-                    <Stack direction={'row'} justify={'center'} spacing={6}>
-                        <Stack align={'center'}>
-                            <Button
-                                variant='ghost'
-                                w={'30'}
-                                mt={2}
-                                rounded={'md'}
-                                leftIcon={<SiLinkedin/>}
-                                _hover={{
-                                transform: 'translateY(-2px)',
-                                boxShadow: 'lg',
-                                }}>
-                            </Button>
-                        </Stack>
-                    </Stack>
-                </Box>          
-            </Grid>
+                    {Card('Zaki GOUIZI','@zackch4n',
+                    'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+                    "Une bonne description de merde",
+                    ["front","back","tout"])}     
+                </SimpleGrid>
+            </VStack>
         </Box>
     </div>
   )
